927ca76cb6e9ae8c4a322338a82c2b5dd7119ecd
hellojin97 <ilhj1228@gmail.com>
2024-03-08 21:37:23 +0900

    커밋할 변경 사항: feat 	수정함:        build.gradle 	수정함:        src/main/java/hello/membermanageservice/SpringConfig.java 	새 파일:       src/main/java/hello/membermanageservice/repository/JdbcMemberRepository.java 	수정함:        src/main/resources/application.properties

--- src/main/java/hello/membermanageservice/SpringConfig.java
+++ src/main/java/hello/membermanageservice/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.membermanageservice;
 
+import hello.membermanageservice.repository.JdbcMemberRepository;
 import hello.membermanageservice.repository.MemberRepository;
 import hello.membermanageservice.repository.MemoryMemberRepository;
 import hello.membermanageservice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
